219afd1d289ca9365a8297694606673c75343f13
Philippe Christophe Baptiste Berset <pberset@c2r7s9.42lausanne.ch>
2024-05-27 14:11:59 +0200

    simpler

--- src/mem/ft_free_int_tab.c
+++ src/mem/ft_free_int_tab.c
@@ -6,7 +6,7 @@
 /*   By: pberset <pberset@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/14 16:36:12 by pberset           #+#    #+#             */
-/*   Updated: 2024/05/27 13:46:05 by pberset          ###   ########.fr       */
+/*   Updated: 2024/05/27 14:11:53 by pberset          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,10 +14,7 @@
 
 void	ft_free_int_tab(int **tab, size_t size)
 {
-	while (size)
-	{
-		free(tab[size - 1]);
-		size--;
-	}
+	while (size--)
+		free(tab[size]);
 	free(tab);
 }
